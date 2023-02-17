ea6a1d9b4bb194338d9911b6a308ef3152f4e6a2
AjahJ <ajahjuliet323@gmail.com>
2023-02-16 16:06:49 -0800

    Best

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,9 +3,7 @@
  * File: 4-puts.c
  * Auth: Benjamin M Musyoki
  */
-
 #include <stdio.h>
-
 /**
  * main - Prints "Programming is like building a multilingual
  *                puzzle, followed by a new line.
@@ -15,6 +13,5 @@
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
-
 return (0);
 }
